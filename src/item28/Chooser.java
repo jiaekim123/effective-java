16218fc096132cc3aa5339bdce71122cdcd9deaf
jiae <37948906+jiaekim123@users.noreply.github.com>
2021-02-09 22:14:36 +0900

    [item28] 아이템 28 수정

---
+++ src/item28/Chooser.java
@@ -0,0 +1,17 @@
+package item28;
+
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Chooser<T> {
+    private final T[] choiceArray;
+
+    public Chooser(Collection<T> choices) {
+        choiceArray = (T[])choices.toArray();
+    }
+
+    public Object choose() {
+        Random rnd = ThreadLocalRandom.current();
+        return choiceArray[rnd.nextInt(choiceArray.length)]);
+    }
+}
