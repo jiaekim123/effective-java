c2f0b6ff6afe7cfb97231ce3a6fd8bf2ac700e8f
jiae <37948906+jiaekim123@users.noreply.github.com>
2021-03-15 20:46:09 +0900

    [item60] item 60 추가

---
+++ src/chapter9/item60/FloatTest.java
@@ -0,0 +1,8 @@
+package chapter9.item60;
+
+public class FloatTest {
+    public static void main(String[] args) {
+        System.out.println(1.03 - 0.42);
+        System.out.println(1.00 - 9 * 0.10);
+    }
+}
